b9a5c3cc868d96f6b99510f181b875242621b57e
locdandcoded <ebony.ashley.brown@gmail.com>
2019-11-24 22:56:58 -0600

    pushed starting outline of html css and js

---
+++ develop/script.js
@@ -0,0 +1,43 @@
+$( document ).ready(function() {
+    var appID = "786b7daf31a3f810fd4b97a983e2a8fe";
+
+    $(".query_btn").click(function(){
+        
+        var query_param = $(this).prev().val();
+        console.log(this)
+
+        if ($(this).prev().attr("placeholder") == "City") {
+            var weather = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param + "&units=imperial&APPID=" + appID;
+        } else if ($(this).prev().attr("placeholder") == "Zip Code") {
+            var weather = "http://api.openweathermap.org/data/2.5/weather?zip=" + query_param + "&units=imperial&APPID=" + appID;
+        }
+
+        $.getJSON(weather,function(json){
+            $("#city").html(json.name);
+            $("#main_weather").html(json.weather[0].main);
+            $("#description_weather").html(json.weather[0].description);
+            $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+            $("#temperature").html(json.main.temp);
+            $("#pressure").html(json.main.pressure);
+            $("#humidity").html(json.main.humidity);
+           
+        });
+    })
+
+    // Optional Code for temperature conversion
+    var fahrenheit = true;
+
+    $("#convertToCelsius").click(function() {
+        if (fahrenheit) {
+            $("#temperature").text(((($("#temperature").text() - 32) * 5) / 9));
+        }
+        fahrenheit = false;
+    });
+
+    $("#convertToFahrenheit").click(function() {
+        if (fahrenheit == false) {
+            $("#temperature").text((($("#temperature").text() * (9/5)) + 32));
+        }
+        fahrenheit = true;
+    });
+});
